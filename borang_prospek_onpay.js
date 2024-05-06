00cc681618a1889f05d6647679f2d98dcddb80be
mfirdausazizi <musafir.kelana@gmail.com>
2024-05-06 11:06:01 +0800

    Update borang_prospek_onpay.js

--- borang_prospek_onpay.js
+++ borang_prospek_onpay.js
@@ -1,38 +1,23 @@
-// Regular expression pattern for Malaysian mobile numbers
-var mobilePattern = /^(01)[0-46-9]-*[0-9]{7,8}$/;
-
+// Submit data to webhook before form submission
 $('#prospect_form').validate({
-  rules: {
-    phone_number: {
-      required: true,
-      pattern: mobilePattern
+    submitHandler: function(form) {
+        // Serialize form data
+        var formData = $(form).serialize();
+        console.log(formData);
+        // Send form data to the external page using AJAX
+        $.ajax({
+            url: webhook_url,
+            type: 'POST',
+            data: formData,
+            success: function(response) {
+                // Handle success response if needed
+                console.log('Form data sent successfully', response);
+                form.submit();
+            },
+            error: function(xhr, status, error) {
+                // Handle error if needed
+                console.log('Error sending form data:', error);
+            }
+        });
     }
-  },
-  messages: {
-    phone_number: {
-      required: "Please enter your phone number",
-      pattern: "Please enter a valid Malaysian mobile number"
-    }
-  },
-  submitHandler: function(form) {
-    // Serialize form data
-    var formData = $(form).serialize();
-    console.log(formData);
-
-    // Send form data to the external page using AJAX
-    $.ajax({
-      url: webhook_url,
-      type: 'POST',
-      data: formData,
-      success: function(response) {
-        // Handle success response if needed
-        console.log('Form data sent successfully', response);
-        form.submit();
-      },
-      error: function(xhr, status, error) {
-        // Handle error if needed
-        console.log('Error sending form data:', error);
-      }
-    });
-  }
 });
